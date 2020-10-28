195b0b7f030ec8a302094337c5e39ad2ac336ba3
Michu0112 <66910540+Michu0112@users.noreply.github.com>
2020-10-28 16:00:32 +0100

    add preloader

---
+++ assets/preloader.js
@@ -0,0 +1,12 @@
+const preload = document.querySelector('.preload');
+const preLogo = document.getElementById('pre-logo');
+preload.classList.add('show-preloader');
+preLogo.classList.add('anim');
+
+window.addEventListener('load', () => {
+    preload.classList.add('fade');
+  	setTimeout(() => {
+        preload.classList.remove('show-preloader');
+        preload.style['display'] = 'none';
+  	}, 1000);
+});
