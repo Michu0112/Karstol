9f3b872e984a23069673782711bf7b8bb1230a9e
Michu0112 <66910540+Michu0112@users.noreply.github.com>
2020-11-07 20:41:15 +0100

    receive photos from server

--- assets/outputtingphotos.js
+++ assets/outputtingphotos.js
@@ -3,10 +3,10 @@ const main = document.querySelector('main');
 
 const outputtingPhotos = () =>{
 
-    for(let i = 7;i < 104;i++){
+    for(let i = 0;i < 104;i++){
     main.innerHTML += `<div>
-    <a href="assets/styles/images/gallery(${i}).jpg" data-lightbox="lightbox-group-1">
-        <div class="lightbox-thumbnail" class="g-min" style="background-image: url('assets/styles/images/gallery(${i}).jpg'); background-repeat: no-repeat;
+    <a href="${Links[i]}" data-lightbox="lightbox-group-1">
+        <div class="lightbox-thumbnail" class="g-min" style="background-image: url('${Links[i]}'); background-repeat: no-repeat;
         background-size: cover;"></div>
     </a>
     </div>`
---
+++ assets/photoBaselLinks.js
@@ -0,0 +1,47 @@
+const Links = [
+    'https://lh3.googleusercontent.com/zzQGsoVFyjDDg9DrXpIQ3hiH4k3T5Xpo5Sb9SXAQJCaSFnoh6w1FE0Ytmn5bmmVFBuckDW8HXcsSBK8nmooySKvs9IHfhIWMTKFY8sRseGgy8pOxWt2pBJJmHioRLTh19Juq-dXjzQ=w2400'
+
+    ,
+
+    'https://lh3.googleusercontent.com/sZHhp2fRG7RFIpSutHzN2AOGJ2PuGzb3nGotKlg4CXcTls9h7sn0xaefoSTmU-pZdhH0aHE6MtwawiK1lNOIf9gmQGNqhg3quSTh_I1YZO4m0eGqyHu49e1dzHRCZfo1_ZXtKODr9Q=w2400'
+
+    ,
+
+    'https://lh3.googleusercontent.com/WF4kHWIrlQNZe6HPfBilOPfCLg526QYuLcTp7RIP3pvvKOC9_WI5mFOcglMD44_kD4AR6l6f50gwd4_cDO2w1j7MUvde67ezwqo1CK9KmlJxfWCwjgbB15focutUX6qKJuSLvWrk8A=w2400'
+
+    ,
+
+    'https://lh3.googleusercontent.com/iWu2pnaxKXFSjh_gv3ShhW6mkNRE7ezpgsYjp--0pt1LBikGcpjLqXpyk2iLqYfWWk6EvcaUfyMp37njkyGezCobWobvUmlNguViwK1KPaG6jBag2shFRCeNLKHbi9deyJZHcqGjVA=w2400'
+
+    ,
+
+    'https://lh3.googleusercontent.com/s7IFhkYS3lMGdymEtj7LAjvjfVp3yQ3KOORZS_8usyBrFFCgou_vU0dIoI_cU2mCQXdnksUHccOWGyDbWOpgxlEsKCbo_ie5yDz3fhxt-hiCNx9nGa_UNs-yJCSWYClPMm5L9wr8lA=w2400'
+
+    ,
+
+    'https://lh3.googleusercontent.com/LrnFLxpjvUU_78mbVLiXF1N34U6ckPwY_QlKJoP0f4cyE0cUNJKkDhnrTQqEXOiNeK-vwmtTDpOBCYK2zChuWlTTRMpk5NttSeN0yl7m4yOmc6Vk_Ak5JPwZmXcZIbVSYMfg9PeV2g=w2400'
+
+    ,
+
+    'https://lh3.googleusercontent.com/lRIXYkKxb3K2idyGcp3JaApBxrrlSj4WNKpB8Sczs_Kcsn9WyKo6VJCQJhZLM6Fe4Qayx3Myf1uim0_ng6mWWuwlh0nGU--lez1iNzt3693V6cWoPMUk0c2Z54ehwryJaE5i3dtUDw=w2400'
+
+    ,
+
+    'https://lh3.googleusercontent.com/Q7p1cLIiUC677snfLKK4jQvPeHzncHgZiteWQ6oVbVYr3Gh9B_oBa7QYakTH1QQTZko1m8aDS4lDv3RToheUHwdA-fAw4i_th-V20_V6Fxnqq4cqU6zL6fwX7EVs0Kb98fuuSf18Wg=w2400'
+
+    ,
+
+    'https://lh3.googleusercontent.com/OeiSAJEBcre7i6EQUyHyOXqqUgJ3FnpInpjDz6gIyL01V1uq-z4eAV8gKYyooyKN-InqGDxEHrt34PqF-N0ZjTITWeVanQhzApPguyQSxL8Azk_CS0lU_cRNYMhZANyZDpdS9CA1lA=w2400'
+
+    ,
+
+    'https://lh3.googleusercontent.com/1VBpPso5tP2Z4c5MsZfUoL0r-uA_8Td10GDk-0xKd3iYT-0L1h6mj5Yc0Ge2sP3rA7aAFUP4XRN9tVR_iqrwjiPsJ2znSZMcdYU4yqQrhL495GMagjMpQRhZWkUAoMzvBo1U_rbdmA=w2400'
+
+    ,
+
+    'https://lh3.googleusercontent.com/AANv7pOzbuVrqyIDHaq2huWrGm4hP_etNHCHlksuc3o8iEtkr6ldj7gxfTWeP8-yiYWtOS0TmR-dd74-DmOU-pLyhHQZvLcRVrHmDxVpm7K6kB0MwyzF6CQuaxsioUNtJRLuWAP-MQ=w2400'
+
+    ,
+
+    
+]
