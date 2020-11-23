f59297101845136ed66ca61e2d716981ac96dd8b
Michu0112 <66910540+Michu0112@users.noreply.github.com>
2020-11-23 20:14:02 +0100

    organise code in css files and speed up preloader

--- babel-project/src/preloader.js
+++ babel-project/src/preloader.js
@@ -3,14 +3,11 @@ const preLogo = document.getElementById('pre-logo');
 preload.classList.add('show-preloader');
 preLogo.classList.add('anim');
 
-const preloader = () =>{
 window.addEventListener('load', () => {
       
     preload.classList.add('fade');
   	setTimeout(() => {
         preload.classList.remove('show-preloader');
         preload.style['display'] = 'none';
-  	}, 900);
+  	}, 800);
 });
-}
-preloader();
