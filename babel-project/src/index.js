2624b16301e4681a2e4710444febb42f98f38645
Michu0112 <66910540+Michu0112@users.noreply.github.com>
2020-11-24 00:01:56 +0100

    last changes side bar

--- babel-project/src/index.js
+++ babel-project/src/index.js
@@ -1,18 +1,23 @@
 const navBar = document.querySelector('.nav-bar');
 const menuBtn = document.querySelector('.menu-btn');
+const body = document.querySelector('body');
 let menuOpen = false;
 const backDrop = document.querySelector('.backDrop');
-menuBtn.addEventListener('click', () =>{
+const triggers = [navBar,menuBtn];
+triggers.forEach((el) =>{
+el.addEventListener('click', () =>{
    if(!menuOpen){
       menuBtn.classList.add('open');
       menuOpen = true;
       navBar.style.right = 0;
+      body.style.overflowY = "hidden";
       backDrop.style.display = 'block';
       setTimeout(() =>{
          backDrop.style.backgroundColor = 'rgba(0, 0, 0, 0.616)';
       },300)
    }
    else{
+      body.style.overflowY = "auto";
       menuBtn.classList.remove('open');
       menuOpen = false;
       navBar.style.right = '-100%';
@@ -22,3 +27,4 @@ menuBtn.addEventListener('click', () =>{
       },300)
    }
 });
+})
