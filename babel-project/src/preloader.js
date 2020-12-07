5f4cf8c6e5797c7a0c1392e0b0a118e3b0153719
Michu0112 <66910540+Michu0112@users.noreply.github.com>
2020-12-07 22:30:04 +0100

    change mechanic of preloader

--- babel-project/src/preloader.js
+++ babel-project/src/preloader.js
@@ -1,17 +1,13 @@
-const body = document.querySelector('body');
-body.insertAdjacentHTML('afterbegin',`<div class="preload" style="font-family: 'Turret Road', cursive;">
-      <div class="circles-group">
-          <div class="logo" id="pre-logo"><span>Kar</span><span class="brown-end">stol</span></div>
-      </div>
-      </div>`);
+
 const preload = document.querySelector('.preload');
 const preLogo = document.getElementById('pre-logo');
 preload.classList.add('show-preloader');
 preLogo.classList.add('anim');
 
+window.addEventListener('load', ()=>{
       preload.classList.add('fade');
   	setTimeout(() => {
         preload.classList.remove('show-preloader');
         preload.style['display'] = 'none';
   	}, 800);
-
+});
