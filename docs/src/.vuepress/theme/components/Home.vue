c95d30e14da01c5d64531a6c13c5d75a4ceb7817
Michał Pachuc <michal.pachuc@sunzinet.com>
2023-09-28 19:51:30 +0200

    feat: refactor code to use vuepress

---
+++ docs/src/.vuepress/theme/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="main-banner">
+        <h1>karstol <br/> twój stolarz</h1>
+    </div>
+</template>
+
+<script>
+export default {
+	name: 'Home',
+};
+</script>
---
+++ docs/src/.vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="theme-container">
+		<Home v-if="$page.frontmatter.home" />
+	</div>
+</template>
+
+<script>
+import Home from '@theme/components/Home.vue';
+export default {
+	name: 'Layout',
+
+	components: {
+		Home,
+	},
+};
+</script>
