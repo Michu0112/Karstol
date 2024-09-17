cf546deba728ac0da619db6ee31388f09ad791a8
Michał Pachuc <michal.pachuc@sunzinet.com>
2024-09-17 14:24:02 +0200

    feat: generate base version static

--- .output/public/_nuxt/62cxU2sb.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,v as c,t as n}from"./dTi51o3U.js";const t={};function r(o,s){return n(),c("div",null," TEST 2 ")}const a=e(t,[["render",r]]);export{a as default};
--- .output/public/_nuxt/Dzhe-Ikh.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./dTi51o3U.js";const r={};function c(n,t){return"TEST 3"}const o=e(r,[["render",c]]);export{o as default};
