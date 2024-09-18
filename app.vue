708d014152396c6dee5eb1dae3b789dbbb91752f
Michał Pachuc <michal.pachuc@sunzinet.com>
2024-09-18 16:52:36 +0200

    feat: test favicon

--- app.vue
+++ app.vue
@@ -48,6 +48,21 @@ onUnmounted(() => {
 
 useHead({
     title: 'Karstol - Profesjonalne schody na zamówienie',
+    link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '32x32',
+            href: '/favicon-32x32.png',
+        },
+        {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '16x16',
+            href: '/favicon-16x16.png',
+        },
+    ],
     meta: [
         {
             name: 'description',
